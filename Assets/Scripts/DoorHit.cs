34f844913cf0a0ef8cae77494ccee791247dedaa
aidazhuma <zaida09@mail.ru>
2015-02-17 11:31:18 -0500

    Collidable environment
    AND THE DOOR

---
+++ Assets/Scripts/DoorHit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorHit : MonoBehaviour {
+	public GameObject scare;
+	public GameObject camera;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("it is hit");
+		scare.transform.position = new Vector3 (camera.transform.position.x + 1, camera.transform.position.y + 1, camera.transform.position.z);
+
+	}
+}
