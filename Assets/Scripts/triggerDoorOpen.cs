93eee3846b1143ec65fa80a8241e0c621845e1bc
hklales <hklales@hampguest65-137.hampshire.edu>
2015-02-18 20:56:26 -0500

    audiooo

--- Assets/Scripts/triggerDoorOpen.cs
+++ Assets/Scripts/triggerDoorOpen.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class triggerDoorOpen : MonoBehaviour {
 	public GameObject door;
 
+	public GameObject audio;
+	NeurosisAudioManager audioManager;
+    
 	// Use this for initialization
 	void Start () {
-	
+		audioManager = audio.GetComponent<NeurosisAudioManager> ();
 	}
 	
 	// Update is called once per frame
@@ -15,6 +18,7 @@ public class triggerDoorOpen : MonoBehaviour {
 	}
 
 	void OnTriggerEnter (Collider other){
+		audioManager.Play (1);
 		Debug.Log ("collision is detected");
 		door.animation.Play("Take 001");
 	}
