8c82f630968715113a3d5328289afc57ec400d27
aidazhuma <zaida09@mail.ru>
2015-02-18 20:07:28 -0500

    Collision with door
    Working collision on the door. But animation doesn’t work

---
+++ Assets/Scripts/triggerDoorOpen.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class triggerDoorOpen : MonoBehaviour {
+	public GameObject door;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider other){
+		Debug.Log ("collision is detected");
+		if(other.gameObject.tag == "OVRPlayerController"){
+			Debug.Log ("the door should open NOW");
+			door.animation.Play("Take 001");
+			
+		}
+	}
+}
