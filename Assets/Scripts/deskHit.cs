d1e1e5b162d4330c4de1ac0c1dca1a3ee007cffd
aidazhuma <zaida09@mail.ru>
2015-02-18 20:24:51 -0500

    Door animation again

---
+++ Assets/Scripts/deskHit.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class deskHit : MonoBehaviour {
+	public GameObject scare;
+	public GameObject camera;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("desk is hit");
+		scare.transform.position = new Vector3 (camera.transform.position.x + 1, camera.transform.position.y + 1, camera.transform.position.z);
+		
+	}
+}
