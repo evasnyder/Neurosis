ef1737e0ee2de45a51a5060f070b81b227b335fb
hklales <hklales@Hannah-Klaless-MacBook-Pro.local>
2015-02-19 23:42:10 -0500

    audio

---
+++ Assets/Scripts/PlayAudio.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAudio : MonoBehaviour {
+    
+    public GameObject audio;
+    NeurosisAudioManager audioManager;
+    
+    public float waitTime = 3.0f;
+    public bool knocked = true;
+    
+    // Use this for initialization
+    void Start () {
+        audioManager = audio.GetComponent<NeurosisAudioManager> ();
+        
+        
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        audioManager.PlayLoop (8);
+        audioManager.PlayLoop (7);
+        Knocking ();
+        
+    }
+    
+    void Knocking(){
+            audioManager.Play (6);
+     
+}
+}
