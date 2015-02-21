81cbca71e701a79472a96939c68d0446fd37a97a
Eva Snyder <snyde24e@mtholyoke.edu>
2015-02-21 16:33:31 -0500

    Audio With Little Girl

--- Assets/Scripts/CameraCollide.cs
+++ Assets/Scripts/CameraCollide.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class CameraCollide : MonoBehaviour {
 	public GameObject scares;
+	public bool ScareHappens = false;
 	Scares jumpScares; 
 	// Use this for initialization
 	void Start () {
@@ -23,6 +24,7 @@ public class CameraCollide : MonoBehaviour {
 		if (gameObject.name == "Desk" && info.collider.name == "OVRPlayerController") {
 			Debug.Log("boo");
 			jumpScares.Appear();
+			ScareHappens = true;
 		}
 	}
 }
--- Assets/Scripts/PlayAudio.cs
+++ Assets/Scripts/PlayAudio.cs
@@ -5,6 +5,9 @@ public class PlayAudio : MonoBehaviour {
     
     public GameObject audio;
     NeurosisAudioManager audioManager;
+
+	public GameObject appear;
+	CameraCollide cameraCollide;
     
     public float waitTime = 3.0f;
     public bool knocked = true;
@@ -12,6 +15,7 @@ public class PlayAudio : MonoBehaviour {
     // Use this for initialization
     void Start () {
         audioManager = audio.GetComponent<NeurosisAudioManager> ();
+		cameraCollide = appear.GetComponent<CameraCollide> ();
         
         
     }
@@ -22,10 +26,14 @@ public class PlayAudio : MonoBehaviour {
         audioManager.PlayLoop (7);
         Knocking ();
         
+		if (cameraCollide.ScareHappens == true) {
+			audioManager.Play (9);
+			cameraCollide.ScareHappens = false;
+		}
     }
     
     void Knocking(){
             audioManager.Play (6);
      
-}
+	}
 }
