e8da3488f0e9edd2a5d92f5b0d1a613a5aae2449
Eva Snyder <snyde24e@mtholyoke.edu>
2015-02-23 19:57:13 -0500

    Map Appears/Disappears

---
+++ Assets/Scripts/Letter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Letter : MonoBehaviour {
+
+	public GameObject letter;
+	bool lookLetter;
+
+	// Use this for initialization
+	void Start () {
+		lookLetter = true; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print ("Update");
+		if (Input.GetKeyDown (KeyCode.Return)) { 
+			print ("ENTER");
+			if(lookLetter) { 
+				//letter.SetActive(false); 
+				letter.GetComponent<MeshRenderer>().enabled = false;
+				lookLetter = false;
+				print ("Bye Bye");
+			} 
+			else {
+				letter.GetComponent<MeshRenderer>().enabled = true;
+				lookLetter = true;
+				print ("Why, Hello there (;" );
+			} 
+		} 
+	}
+
+}
