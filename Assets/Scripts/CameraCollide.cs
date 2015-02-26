901c97ce4f3cd213fac10df9a49a226fc6ca8d75
aidazhuma <zaida09@mail.ru>
2015-02-25 21:58:50 -0500

    Player Respawn + No Fog in first room
    No fog in first room until the door is opened.
    Player respawns in the first room when going up the stairs.

--- Assets/Scripts/CameraCollide.cs
+++ Assets/Scripts/CameraCollide.cs
@@ -8,7 +8,7 @@ public class CameraCollide : MonoBehaviour {
 	*	appear at the door when you hit it.
 	**/
 	public GameObject scares;
-
+	public bool hasScares;
 	/* 
 	 *  This is used for the audio.  This boolean is used in PLAYAUDIO.CS to play teh audio when 
 	 * 	this is set equal to true. i.e something hit the object and the scare appeared.
@@ -24,9 +24,11 @@ public class CameraCollide : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		if (hasScares) {
+			//initiallizing jumpScares to be of type SCARES.CS 
+			jumpScares = scares.GetComponent<Scares> ();
+		}
 
-		//initiallizing jumpScares to be of type SCARES.CS 
-		jumpScares = scares.GetComponent<Scares> ();
 	}
 	
 	// Update is called once per frame
@@ -36,7 +38,7 @@ public class CameraCollide : MonoBehaviour {
 
 	void OnTriggerEnter(Collider info)
 	{
-		print("Detected collision between " + gameObject.name + " and " + info.collider.name);
+		print("Detected collision between " + gameObject.name + " and " + info.collider.name + " in CameraCollide");
 
 		//if the object that was collided with was the desk and what collided with it 
 		//was the player 
@@ -50,14 +52,25 @@ public class CameraCollide : MonoBehaviour {
 			//with the girl appearing
 			deskScareHappens = true;
 		}
-
+		else if (gameObject.name == "RespawnPoint" && info.collider.name == "OVRPlayerController"){ 
+			print("player should respawn");
+			StartCoroutine(respawn(info)); 
+		}
 		//KEEP THIS.  HANNAH IS WORKING ON GETTING THE KNOCKING WORKING WHEN YOU HIT FRAME02 (aka fred.)
-//		if (gameObject.name == "Frame02" && info.collider.name == "OVRPlayerController") {
-//			Invoke("Knocking",1.0f);
-//		}
-	}
+		//		if (gameObject.name == "Frame02" && info.collider.name == "OVRPlayerController") {
+		//			Invoke("Knocking",1.0f);
+		//		}
+
 //
 //	void Knocking(){
 //		audio.Play ();
 //	}
+}
+	IEnumerator respawn(Collider toRespawn){
+		renderer.enabled = false;
+		yield return new WaitForSeconds(2.0f);
+		toRespawn.transform.position = new Vector3(0.04833326f, 3.980667f, 0.0f);
+		toRespawn.transform.rotation = Quaternion.identity;
+		renderer.enabled = true;
+	}
 }
--- Assets/Scripts/firstDoorToOpen.cs
+++ Assets/Scripts/firstDoorToOpen.cs
@@ -1,31 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class triggerDoorOpen : MonoBehaviour {
-	public GameObject door;
+public class firstDoorToOpen : MonoBehaviour {
 
+	public GameObject door;
 	public GameObject audio;
 	NeurosisAudioManager audioManager;
-    
+	private bool fogOn;
+	private int counter = 0;
+	
 	// Use this for initialization
 	void Start () {
 		audioManager = audio.GetComponent<NeurosisAudioManager> ();
-
+		fogOn = false;
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (fogOn) {
+			// Enable fog
+			RenderSettings.fog = true;
+		}
 	}
-
+	
 	IEnumerator OnTriggerEnter (Collider other){
 		audioManager.Play (1);
 		print("open door");
 		door.animation["Take 001"].speed = 1;
 		door.animation.Play("Take 001");
-		
+		fogOn = true;
+
 		yield return new WaitForSeconds(5);
-		
+
+
 		print("close door");
 		door.animation["Take 001"].speed = -1;    
 		door.animation.Play("Take 001");
