3f974b86eed4d293e26f9be16ed4ee5ecc99d67b
aidazhuma <zaida09@mail.ru>
2015-03-03 01:00:50 -0500

    Shrinking hall

--- Assets/Scripts/movingWall.cs
+++ Assets/Scripts/movingWall.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class movingWall : MonoBehaviour {
 	public GameObject wallToMove;
 	private bool triggered;
+	private bool alreadyTriggered;
 	private Vector3 endPosition = new Vector3(15f, 8f, -47.9f);
 
 
@@ -11,13 +12,16 @@ public class movingWall : MonoBehaviour {
 	void Start () {
 		wallToMove.active = false;
 		triggered = false;
+		alreadyTriggered = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (triggered == true)
+		if (triggered == true && alreadyTriggered == false)
 		{
-			StartCoroutine(WaitAndMove(1));
+			alreadyTriggered= true;
+			//StartCoroutine(WaitAndMove(1));
+			Invoke ("StartCoRoutine", 3);
 			}
 			if (wallToMove.transform.position == endPosition)
 			{
@@ -25,14 +29,20 @@ public class movingWall : MonoBehaviour {
 				//Destroy (gameObject);
 			}
 		}
+	void StartCoRoutine(){
+		StartCoroutine(WaitAndMove ());
 
-	IEnumerator WaitAndMove(float delayTime){
-		yield return new WaitForSeconds(delayTime); // start at time X
-		float startTime=Time.time; // Time.time contains current frame time, so remember starting point
-		while(Time.time-startTime<=100){ // until one second passed
-			wallToMove.transform.position = Vector3.Lerp(wallToMove.transform.position, endPosition,Time.time-startTime); // lerp from A to B in one second
-			yield return 1; // wait for next frame
+	}
+
+	IEnumerator WaitAndMove(){
+		float i = 0.0f;
+		//float rate = 1.0f/100f;
+		while (i < 100f) {
+		//	i += Time.deltaTime * rate;
+			wallToMove.transform.position = Vector3.Lerp(wallToMove.transform.position, endPosition, 0.001f);			
+			yield return 1; 
 		}
+			
 	}
 
 	void OnTriggerEnter(Collider info)
@@ -43,8 +53,14 @@ public class movingWall : MonoBehaviour {
 
 		}
 		if (gameObject.name == "HangingLight_MoveWall" && info.collider.name == "OVRPlayerController") { 
-			print ("wall starts moving");
+			print ("wall should start moving");
 			triggered= true;
+		}
+		if (gameObject.name == "Wall_Moving" && info.collider.name == "OVRPlayerController") { 
+			print ("player should be moved");
+			//info.transform.Translate(new Vector3( -0.5f, 0.0f, 0.0f));
+			//info.transform.rotation = Quaternion.identity;
+
 		}
 	}
 }
