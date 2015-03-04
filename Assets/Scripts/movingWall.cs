93452cfd52b883d6f827a662b687f8d28ce34230
aidazhuma <zaida09@mail.ru>
2015-03-04 13:02:23 -0500

    Wall moving normal speed
    Wall now moving with the normal slow speed, but it still passes through
    the player.

--- Assets/Scripts/movingWall.cs
+++ Assets/Scripts/movingWall.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class movingWall : MonoBehaviour {
 	public GameObject wallToMove;
 	private bool triggered;
+	private bool alreadyTriggered;
 	private Vector3 endPosition = new Vector3(15f, 8f, -47.9f);
 
 
@@ -11,13 +12,15 @@ public class movingWall : MonoBehaviour {
 	void Start () {
 		wallToMove.active = false;
 		triggered = false;
+		alreadyTriggered = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (triggered == true)
+		if (triggered == true && alreadyTriggered == false)
 		{
-			StartCoroutine(WaitAndMove(1));
+			alreadyTriggered= true;
+			Invoke ("StartCoRoutine", 3);
 			}
 			if (wallToMove.transform.position == endPosition)
 			{
@@ -26,12 +29,17 @@ public class movingWall : MonoBehaviour {
 			}
 		}
 
-	IEnumerator WaitAndMove(float delayTime){
-		yield return new WaitForSeconds(delayTime); // start at time X
-		float startTime=Time.time; // Time.time contains current frame time, so remember starting point
-		while(Time.time-startTime<=100){ // until one second passed
-			wallToMove.transform.position = Vector3.Lerp(wallToMove.transform.position, endPosition,Time.time-startTime); // lerp from A to B in one second
-			yield return 1; // wait for next frame
+	void StartCoRoutine(){
+	StartCoroutine(WaitAndMove ());
+	
+	}
+	IEnumerator WaitAndMove(){
+		float i = 0.0f;
+		//float rate = 1.0f/100f;
+		while (i < 100f) {
+			//	i += Time.deltaTime * rate;
+			wallToMove.transform.position = Vector3.Lerp(wallToMove.transform.position, endPosition, 0.001f);			
+			yield return 1; 
 		}
 	}
 
