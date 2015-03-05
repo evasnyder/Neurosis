7b7fc0d5220048e3205510af8e4f6fa9841ff6ee
Eva Snyder <snyde24e@mtholyoke.edu>
2015-03-04 21:03:29 -0500

    Lights Fucking

--- Assets/Scripts/Room1PickUp.cs
+++ Assets/Scripts/Room1PickUp.cs
@@ -5,20 +5,84 @@ public class Room1PickUp : MonoBehaviour {
 
 	public GameObject note;
 	public GameObject player;
-	public GameObject lightTest;
+	public GameObject light1;
+	public GameObject light2;
+	public GameObject light3;
 	//public Light myLight;
 
-	float lightStep = .05f;
+	float lightStep = .1f;
+	float thirdLight = 5f;
+	float fucker = 1000;
+	int fuckerCounter = 0;
+
 	bool pickedUp = false;
+	bool turnLight1 = false; 
+	bool turnLight2 = false; 
+	bool turnLight3 = false; 
+	bool lightFlucker = false;
 	
 	// Use this for initialization
 	void Start () {
 		light.GetComponent("Light");
+		light2.light.intensity = 0.0f;
+		print ("Turn light 2 off in beginning");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (turnLight1) { 
+			light1.light.intensity -= lightStep * Time.deltaTime;
+			//lightTest.light.intensity = 0.0f;
+			print ("intensity: " + light1.light.intensity);
+
+			if(light1.light.intensity <= 0.1) { 
+				turnLight2 = true;
+			} 
+		} 
+
+		if (turnLight2) { 
+			light2.light.intensity = .5f;
+			print("Turn Light 2 On");
+
+			turnLight3 = true; 
+		} 
+
+		if (turnLight3) { 
+			if(thirdLight > 1){ 
+				print ("Time Decrease");
+				thirdLight -= Time.deltaTime;
+				print ("ThridLight Time :" + thirdLight);
+			} 
+				
+			else { 
+				print("Turn Light 3 On");
+				light3.light.intensity = .5f;
+
+				float time = Time.deltaTime;
+
+				if(Time.deltaTime > time) { 
+					fuckerCounter++;
+					print ("fuckerCounter" + fuckerCounter);
+					lightFlucker = true;
+				} 
+
+				//yield WaitForSeconds(1); 
+				if(fuckerCounter%2.0f ==0){
+					Debug.Log ("fucker has fucked");
+					light3.light.intensity =1.0f;
+					fucker -=Time.deltaTime;
+				}
+				else{
+					fucker -=Time.deltaTime;
+					Debug.Log ("fucker is waiting");
+					print ("fucker: " + fucker);
+					light3.light.intensity =0.0f;
+				}
+			} 
+		}
+
+
+
 	}
 
 	void OnTriggerEnter(Collider info)
@@ -31,16 +95,8 @@ public class Room1PickUp : MonoBehaviour {
 			//if(!pickedUp){
 			//note.transform.Translate (player.transform.position.x+1, 0, player.transform.position.z+1); //= 
 			note.transform.localPosition= new Vector3(.44f,-.498f,.65f);
-			
-			//new Vector3(-.17f, -.498f,.65f);
-			//	pickedUp = true;
-			//}
-			
-			//TURN THE LIGHT OFF SLOWLY
-			lightTest.light.intensity -= lightStep * Time.deltaTime;
-			//lightTest.light.intensity = 0.0f;
-			print ("lights off");
-			print ("intensity: " + lightTest.light.intensity);
+
+			turnLight1 = true; 
 		}
 		
 	}
