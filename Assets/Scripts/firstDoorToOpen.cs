e6dfd22c587b626b721c5c1aa120b3e989c740e3
aidazhuma <zaida09@mail.ru>
2015-03-09 20:04:43 -0400

    Windows are transparent+loop fog+ door not opening multiple times

--- Assets/Scripts/firstDoorToOpen.cs
+++ Assets/Scripts/firstDoorToOpen.cs
@@ -8,6 +8,7 @@ public class firstDoorToOpen : MonoBehaviour {
 	NeurosisAudioManager audioManager;
 	private bool fogOn;
 	private int counter = 0;
+	private bool isColliding;
 
 	public bool doorHit = false; //used for playaudio.cs
 	public bool doorSlammed = false; //also used for audio
@@ -27,21 +28,25 @@ public class firstDoorToOpen : MonoBehaviour {
 		}
 	}
 	
-	IEnumerator OnTriggerEnter (Collider other){
-		audioManager.Play (1);
-		//Invoke ("DoorSlam", 2.0f);
-		doorHit = true;
-		print("open door");
-		door.animation["Take 001"].speed = 1;
-		door.animation.Play("Take 001");
-		fogOn = true;
-		yield return new WaitForSeconds(5);
+	void OnTriggerEnter (Collider other){
+		if(other.collider.name == "OVRPlayerController"){
+			audioManager.Play (1);
+			doorHit = true;
+			print ("open door");
+			door.animation ["Take 001"].speed = 1;
+			door.animation.Play ("Take 001");
+			fogOn = true;
+		}
 
+	}
 
-		print("close door");
-		door.animation["Take 001"].speed = -1;    
-		door.animation.Play("Take 001");
-		doorSlammed = true;
+	void onTriggerExit(Collider other){
+		if (other.collider.name == "OVRPlayerController") {
+			print ("close door");
+			door.animation ["Take 001"].speed = -1;    
+			door.animation.Play ("Take 001");
+			doorSlammed = true;
+		}
 	}
 
 	void DoorSlam(){
