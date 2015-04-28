d33c122f92cd296dfd308bdf6d1b28fcb6222e1e
aidazhuma <zaida09@mail.ru>
2015-04-28 15:07:33 -0400

    Axe not working

---
+++ Assets/Scripts/axFly.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class axFly : MonoBehaviour {
+	
+	bool follow = false;
+	
+	GameObject sticky_wall;
+	
+	public float speed = 10;
+	
+	Vector3 temp;
+	
+	GameObject player;
+	
+	// Use this for initialization
+	void Start () {
+		
+		player = GameObject.Find ("OVRPlayerController");
+		sticky_wall = GameObject.Find ("Wall_Moving");
+		
+		Physics.IgnoreCollision(gameObject.collider, player.collider);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+		if (follow == false) {
+			
+			rigidbody.velocity = new Vector3 (speed, 0, 0);
+			
+		} else {
+			
+			
+			//temp = sticky_wall.transform.position;
+			//temp.y = transform.position.y;
+			//temp.z = transform.position.z;
+
+			//transform.position = temp;
+
+		}
+		
+	}
+	
+	
+	void OnCollisionEnter(Collision collide)
+	{    
+		if(collide.collider.gameObject.name == "Wall_Moving")        
+		{
+			var joint = gameObject.AddComponent<FixedJoint>(); joint.connectedBody = collide.rigidbody; 
+			follow = true;
+			//Destroy (gameObject.rigidbody);
+		}        
+	}
+}
---
+++ Assets/Scripts/axScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class axScript : MonoBehaviour {
+
+	public GameObject axPrefab;
+
+	bool triggered = false;
+
+	public Vector3 startPos = new Vector3(9,9,-47);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+	void OnTriggerEnter(){
+
+		if (!triggered) {
+
+			Instantiate (axPrefab, startPos, Quaternion.identity);
+
+			triggered = true;
+
+			
+		}
+
+	}
+}
